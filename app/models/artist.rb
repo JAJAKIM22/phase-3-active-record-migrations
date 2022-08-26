fca95ef32a3f77fcf43737f85dfb798cce16ec7a
Peter Kimani <peterjajakim@gmail.com>
2022-08-26 13:32:42 +0300

    LAB COMPLETE

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,1 +1,15 @@
 # add the Artist class here
+class Artist < ActiveRecord::Base
+end
+
+class AddFavoriteFoodToArtists < ActiveRecord::Migration[6.1]
+    def change
+      add_column :artists, :favorite_food, :string
+    end
+  end
+
+  class AddFavoriteFlowerToArtists < ActiveRecord::Migration[6.1]
+    def change
+      add_column :artists, :favorite_flower, :string
+    end
+  end
